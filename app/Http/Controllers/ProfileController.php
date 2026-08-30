aa72e5a918cd5c969cf0c0bb68e6444ef884089a
Langitz <ffaiz7333@gmail.com>
2026-08-30 10:35:32 +0700

    Reapply "CRUD Profile"
    This reverts commit aef8af39a34b6bea8c56dacd83ebac98a3758617.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,8 +3,50 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ProfileController extends Controller
 {
-    //
+    public function index()
+    {
+        $user = auth()->user();
+
+        return view('profile', [
+            'user' => $user,
+        ]);
+    }
+
+    public function update(Request $request)
+    {
+        $user = auth()->user();
+
+        $data = $request->validate([
+            'name' => [
+                'required',
+                'string',
+                'max:255',
+            ],
+            'email' => [
+                'required',
+                'email',
+                'max:255',
+                Rule::unique('users', 'email')->ignore($user->id),
+            ],
+            'telepon' => [
+                'nullable',
+                'string',
+                'max:20',
+            ],
+            'alamat' => [
+                'nullable',
+                'string',
+            ],
+        ]);
+
+        $user->update($data);
+
+        return redirect()
+            ->route('profile')
+            ->with('success', 'Profil berhasil diperbarui');
+    }
 }
